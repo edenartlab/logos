c481e57fc50b50bbc474fe2b790da8d3ee3dd5ef
Gene Kogan <kogan.gene@gmail.com>
2023-10-17 16:11:55 -0700

    md + txt

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
+from glob import glob
+
+md_files = [f.split('/', 1)[1] for f in glob('logos/**/*.md', recursive=True)]
+txt_files = [f.split('/', 1)[1] for f in glob('logos/**/*.txt', recursive=True)]
 
 setup(
     name='eden-logos',
     version='0.0.1',
     packages=find_packages(),
     install_requires=['simpleaichat', 'python-dotenv', 'pytest'],
-    package_data={'': ['*.txt', '*.md']}
+    package_data={'logos': md_files + txt_files}
 )
